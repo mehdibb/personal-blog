3c5b100a738805925faac870ff97184637ec5694
mehdibb <mehdi.babapour@zardalu.dev>
2020-12-04 21:27:56 +0330

    some main app shell fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps): React.ReactElement {
+  return (
+    <>
+      {/* Whole app wrapper with background color */}
+      <div className="bg-light main_wrapper">
+        {/* the main responsive container */}
+        <div className="container">
+          {/* the whole app column */}
+          <div className="col-sm">
+            {/* topbar including logo and hamburger menu */}
+            <div className="row" />
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import styles from './index.module.scss';
 
 export default function Home(): React.ReactElement {
   return (
-    <main className={`${styles.main} container d-flex justify-content-center align-items-center`}>
-      <h1>React/Next/Bootstrap</h1>
-    </main>
+    <>
+      home
+    </>
   );
 }
